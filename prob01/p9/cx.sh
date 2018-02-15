03f4f06ce357f04ce8703f495d14f1a3e528dfe0
Miguel Duarte <gamer-go@hotmail.com>
2018-02-15 10:53:50 +0000

    Also added output redirecting to dev null in 9a

--- prob01/p9/cx.sh
+++ prob01/p9/cx.sh
@@ -9,11 +9,14 @@ then
 	exit 1
 fi
 
+# &> means redirecting stdout (>) and stderr (&) to chosen file
+# /dev/null is basically a black hole, anything that is written there is deleted. So we are simply "trashing" the compilation output
+
 # Removing compiled file
-rm -f $1.o
+rm -f $1.o &> /dev/null
 
 # Compiling the corresponding .c file
-gcc -Wall -o $1.o $1.c
+gcc -Wall -o $1.o $1.c &> /dev/null
 
 # Checking for successful compilation
 if [[ ! $? -eq 0 ]]
